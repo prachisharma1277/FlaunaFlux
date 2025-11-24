f4a8f80e4c5b96651952f508499538b45743442f
Prachi Sharma <145744960+prachisharma1277@users.noreply.github.com>
2025-11-24 22:19:20 +0530

    Update mail.js

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -37,7 +37,7 @@ async function sendAlertEmail(alert) {
   try {
     const info = await transporter.sendMail({
       from: '"FaunaFlux Alerts" <prachi@mail.vinucode.in>', // Your "from" address
-      to: ["prachi141btcse23@igdtuw.ac.in","prachisharmaa1277@gmail.com"] ,// Your recipient list
+      to: ["prachi141btcse23@igdtuw.ac.in","kajal082btcse23@igdtuw.ac.in"] ,// Your recipient list
       subject: subject,
       text: textBody,
     });
@@ -51,3 +51,4 @@ async function sendAlertEmail(alert) {
 // 3. Export the function
 
 module.exports = { sendAlertEmail };
+
